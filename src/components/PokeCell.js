d0ec808ff2852837a9f1f74b182ad47711c4db69
rafanetow <rafanetow@gmail.com>
2019-07-29 02:04:15 -0700

    added sprites to pokemons

--- src/components/PokeCell.js
+++ src/components/PokeCell.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './styles/PokeCell.css';
+import sprites from '../assets/sprites.png';
 
 
-const PokeCell = () => {
-  return <button className="poke-cell"></button>
+const PokeCell = ({pokeClass}) => {
+    const { id, backgroundPosition } = pokeClass;
+    const style = { backgroundImage: `url(${sprites})`,backgroundPosition };
+  return <button style = {style} className="poke-cell"></button>
 };
 
 
