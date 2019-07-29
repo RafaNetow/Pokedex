8b6cc67b51775289b20ecf77e2bfc6506d49d844
rafanetow <rafanetow@gmail.com>
2019-07-29 12:48:50 -0700

    added git ignore and dateilview component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import PokeList from './PokeList'
+import DetailView from './DetailView'
 import './styles/App.css';
 
 
 class App extends Component {
+
+  handlerOnClick(id) {
+    console.log(id);
+  }
   render() {
     return (
       <div className="App">
         <PokeList/>
+         <DetailView />
       </div>
     );
   }
