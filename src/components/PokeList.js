0a3b5616e3a4558845a64edd42562cf69ef2d6db
rafanetow <rafanetow@gmail.com>
2019-07-29 01:06:27 -0700

    added poke cell

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import PokeCell from './PokeCell'
 import './styles/PokeList.css';
 
 const PokeList = () => {
     return (
     <section className="poke-list">
+    <PokeCell/>
+    <PokeCell/>
+    <PokeCell/>
+
 
     </section>
     )
